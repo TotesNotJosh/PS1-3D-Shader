5d9efb7bda6be9b4f77e66b63fe46608ca22c2c6
Not Josh <161392075+TotesNotJosh@users.noreply.github.com>
2025-01-05 08:20:30 -0600

    Add ShaderGUI file

---
+++ PS1 Shader GUI.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class PS1ShaderGUI : ShaderGUI
+{
+    public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
+    {
+        // Find properties
+        MaterialProperty useIntFog = FindProperty("_UseIntFog", properties);
+        MaterialProperty fogSteps = FindProperty("_FogSteps", properties);
+
+        // Draw default properties
+        foreach (MaterialProperty property in properties)
+        {
+            if (property.name == "_FogSteps") {
+                // Only show _FogSteps if _UseIntFog is enabled
+                if (useIntFog.floatValue != 0.0f)
+                {
+                    materialEditor.ShaderProperty(property, property.displayName);
+                }
+            } else if (property.name == "_Shininess") {
+                // Hide _Shininess property
+                continue;
+            } else {
+                materialEditor.ShaderProperty(property, property.displayName);
+            }
+        }
+    }
+}
